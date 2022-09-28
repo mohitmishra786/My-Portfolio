2a3f083d3f7f4bdd07ce70b713f7a13552aa3ae7
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-28 22:07:56 +0530

    Update

--- my-portfolio/components/Header.tsx
+++ my-portfolio/components/Header.tsx
@@ -6,8 +6,39 @@ type Props = {}
 function Header({}: Props) {
   return (
     <header>
+        <div className='flex flex-row items-center'>
+          {/*Social Icons */}
+          <SocialIcon 
+          url="https://twitter.com/jaketrent"
+          fgColor='gray'
+          bgColor='transparent' 
+          />
+          <SocialIcon 
+          url="https://twitter.com/jaketrent"
+          fgColor='gray'
+          bgColor='transparent' 
+          />
+          <SocialIcon 
+          url="https://twitter.com/jaketrent"
+          fgColor='gray'
+          bgColor='transparent' 
+          />
+          <SocialIcon 
+          url="https://twitter.com/jaketrent"
+          fgColor='gray'
+          bgColor='transparent' 
+          />
+        </div>
+
         <div>
-          {/*Social Icons */}<SocialIcon url="https://twitter.com/jaketrent" />
+          <SocialIcon 
+            className='cursor-pointer'
+            network='email'
+            fgColor='gray'
+            bgColor='transparent' 
+          />
+          <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>
+            Get In Touch</p>
         </div>
     </header>
   )
