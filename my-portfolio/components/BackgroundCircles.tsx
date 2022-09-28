a28e97906299280c7c04265a4b726ca7f18d1e6e
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-28 23:10:09 +0530

    Update

--- my-portfolio/components/BackgroundCircles.tsx
+++ my-portfolio/components/BackgroundCircles.tsx
@@ -4,12 +4,12 @@ type Props = {}
 
 function BackgroundCircles({}: Props) {
   return (
-    <div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+    <div className='relative flex justify-center items-center'>
+        <div className= "absolute border [#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping " />
+        <div className= "absolute border [#333333] rounded-full h-[300px] w-[300px] mt-52 animate-ping " />
+        <div className= "absolute border [#333333] rounded-full h-[500px] w-[500px] mt-52 animate-ping " />
+        <div />
+        <div />
     </div>
   )
 }
