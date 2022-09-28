c5bd93b91fd02a9ab2e120bc021cde1487497441
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-28 23:15:51 +0530

    Update

--- my-portfolio/components/BackgroundCircles.tsx
+++ my-portfolio/components/BackgroundCircles.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion } from "framer-motion";
 
 type Props = {}
 
@@ -6,10 +7,10 @@ function BackgroundCircles({}: Props) {
   return (
     <div className='relative flex justify-center items-center'>
         <div className= "absolute border [#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping " />
-        <div className= "absolute border [#333333] rounded-full h-[300px] w-[300px] mt-52 animate-ping " />
-        <div className= "absolute border [#333333] rounded-full h-[500px] w-[500px] mt-52 animate-ping " />
-        <div />
-        <div />
+        <div className= "rounded-full border border-[#333333]  h-[300px] w-[300px] mt-52 animate-ping " />
+        <div className= "rounded-full border border-[#333333]  h-[500px] w-[500px] mt-52 animate-ping " />
+        <div className= "rounded-full border border-[#F7AB0A] opacity-20 h-[650px] w-[650px] absolute mt-52 animate-pulse"/>
+        <div className= "rounded-full border border-[#333333] h-[800px] w-[800px] absolute mt-52"/>
     </div>
   )
 }
