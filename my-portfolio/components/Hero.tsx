77bd7c88fe94915601e4981b2a7217f23a4190cd
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-28 23:41:26 +0530

    Update

--- my-portfolio/components/Hero.tsx
+++ my-portfolio/components/Hero.tsx
@@ -9,7 +9,7 @@ function Hero({}: Props) {
         words: [
             "Hi, My name is Mohit Mishra",
             "Guy Who Love Coffee with ML", 
-            "Who Loves to Code Alot",
+            "But Loves to Code Alot",
         ],
         loop: true,
         delaySpeed: 2000,
@@ -26,10 +26,25 @@ function Hero({}: Props) {
         />
 
         <div>
-            <h1>
-                <span>{text}</span>
+            <h2 className='text-sm text-gray-500 uppercase pb-2 tracking-[15px]'>
+                Software Engineer
+            </h2>
+            <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+                <span className='mr-3'>{text}</span>
                 <Cursor cursorColor="#F7AB0A" />
             </h1>
+
+            <div>
+                <button>
+
+                </button>
+                <button>
+
+                </button>
+                <button>
+                    
+                </button>
+            </div>
         </div>
         
     </div>
