d3803f85fa0b8fc0034ee3aa3295e26a14f91290
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 01:17:51 +0530

    Update

--- my-portfolio/components/About.tsx
+++ my-portfolio/components/About.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { motion } from "framer-motion";
+
+// https://avatars.githubusercontent.com/u/71754779?v=4
 
 type Props = {}
 
 const About = (props: Props) => {
   return (
     <div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-        <h3 className='absolute top-24 uppercase tracking-[20px]  '>About</h3>
+        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+          About
+        </h3>
+        <motion.img 
+        src= "https://avatars.githubusercontent.com/u/71754779?v=4"
+        />
     </div>
   )
 }
