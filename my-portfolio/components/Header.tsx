ac60055c7024010cacfabbf5430c3a5fba355fb7
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-28 20:49:32 +0530

    Update in the index.ts

---
+++ my-portfolio/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function Header({}: Props) {
+  return (
+    <div>I am a Header</div>
+  )
+}
+
+export default Header
--- my-portfolio/pages/index.tsx
+++ my-portfolio/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Header from '../components/Header'
 
 
 const Home: NextPage = () => {
@@ -13,7 +14,7 @@ const Home: NextPage = () => {
 
 
       {/* Hero */}
-
+      <Header />
 
       {/* About Me */}
 
