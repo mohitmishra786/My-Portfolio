19084f0613e567e05b6da6201eac2a593278fed4
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 01:10:31 +0530

    Update

--- my-portfolio/components/About.tsx
+++ my-portfolio/components/About.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const About = (props: Props) => {
   return (
-    <div className='flex flex-col relative h-screen text-center md:text-left md:flex-row px-10 justify-evenly mx-auto items-center'>
+    <div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
         <h3 className='absolute top-24 uppercase tracking-[20px]  '>About</h3>
     </div>
   )
