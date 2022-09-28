4e07c5c12e5caa46c14d43383af4391181725e34
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 01:20:43 +0530

    Update

--- my-portfolio/components/About.tsx
+++ my-portfolio/components/About.tsx
@@ -11,7 +11,21 @@ const About = (props: Props) => {
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
           About
         </h3>
-        <motion.img 
+        <motion.img
+        initial={{
+          x: -200,
+          opacity: 0,
+        }} 
+        transition={{
+          duration: 1.2,
+        }}
+        whileInView={{
+          x:0,
+          opacity: 1,
+        }}
+        viewport={{
+          once: true,
+        }}
         src= "https://avatars.githubusercontent.com/u/71754779?v=4"
         />
     </div>
