29826bd8574445669b6be685ffdbf51c284f580d
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 00:17:24 +0530

    Update

--- my-portfolio/components/About.tsx
+++ my-portfolio/components/About.tsx
@@ -4,7 +4,9 @@ type Props = {}
 
 const About = (props: Props) => {
   return (
-    <div className='"h-screen'>About</div>
+    <div className='flex flex-col relative h-screen text-center md:text-left md:flex-row px-10 justify-evenly mx-auto items-center'>
+        About
+    </div>
   )
 }
 
