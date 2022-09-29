5fb9bffeffeb8f7976064eac0ee76cdc66606c02
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 16:12:51 +0530

    Update

--- my-portfolio/components/Projects.tsx
+++ my-portfolio/components/Projects.tsx
@@ -6,7 +6,19 @@ type Props = {}
 function Projects({}: Props) {
   const projects = [1, 2, 3, 4, 5];
   return (
-    <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0 '>
+    <motion.div 
+        initial={{
+            opacity: 0,
+        }}
+        whileInView={{
+            opacity: 1,
+        }}
+
+        transition={{
+            duration: 1.5,
+        }}
+        
+        className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0 '>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
             Projects
         </h3>
@@ -14,8 +26,21 @@ function Projects({}: Props) {
             {projects.map((project , i) => (
                 <div 
                     className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
-                    <img 
-                    className='w-56 h-36'
+                    <motion.img
+                    initial={{
+                        y: -300,
+                        opacity: 0,
+                    }}
+                    whileInView={{
+                        opacity: 1,
+                        y: 0,
+                    }}
+                    transition={{
+                        duration: 1.5,
+                    }}
+                    viewport={{
+                        once: true,
+                    }}
                     src="https://image.freepik.com/vector-gratis/dibujos-animados-personas-hogar-planas-hombre-yace-banco-calle-tiene-frio_1284-33044.jpg" 
                     alt="" />
                    <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
@@ -30,7 +55,7 @@ function Projects({}: Props) {
             ))}
         </div>
         <div className='w-full absolute top-[30%] bg-[#F7Ab0A]/10 left-0 h-[500px] -skew-y-12' />
-    </div>
+    </motion.div>
   );
 }
 
