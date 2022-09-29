e6d4dd6fb3c0a727e2d7123c27e16a4d2d5d27da
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 12:00:21 +0530

    Update

--- my-portfolio/components/About.tsx
+++ my-portfolio/components/About.tsx
@@ -9,6 +9,7 @@ export default function About({}: Props) {
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
           About
         </h3>
+        
         <motion.img
           initial={{
             x: -200,
@@ -24,8 +25,8 @@ export default function About({}: Props) {
           viewport={{
             once: true,
           }}
-        src="https://media-exp1.licdn.com/dms/image/C4D03AQG3ezW14WyKhQ/profile-displayphoto-shrink_200_200/0/1662836126839?e=1669852800&v=beta&t=UOmovv1TESmdfv8VigmHIiC4tduLsl4JhCF-eSIyF5Q" 
-       // src="https://static.remove.bg/remove-bg-web/3ad3b721d276f1af1fb7121aff638a866139749a/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg"
+          src="https://media-exp1.licdn.com/dms/image/C4D03AQG3ezW14WyKhQ/profile-displayphoto-shrink_200_200/0/1662836126839?e=1669852800&v=beta&t=UOmovv1TESmdfv8VigmHIiC4tduLsl4JhCF-eSIyF5Q" 
+    
           className="-mb-20 md:mb-0 flex-shrink-0 rounded-full w-56 h-56 object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
         />
     </div>
