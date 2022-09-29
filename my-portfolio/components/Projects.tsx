e0b7b2ea8e769193e0bb71154dee550da697352b
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 15:12:31 +0530

    Update

--- my-portfolio/components/Projects.tsx
+++ my-portfolio/components/Projects.tsx
@@ -4,12 +4,12 @@ type Props = {}
 
 function Projects({}: Props) {
   return (
-    <div className='h-screen'>
+    <div className='h-screen relative flex overflow-hidden felx-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0 '>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
             Projects
         </h3>
     </div>
-  )
+  );
 }
 
 export default Projects
