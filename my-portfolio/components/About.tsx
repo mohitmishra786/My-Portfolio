adf4dea8fe6b82d4f7440a7caff3cd287d8ed26c
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 12:06:39 +0530

    Update

--- my-portfolio/components/About.tsx
+++ my-portfolio/components/About.tsx
@@ -1,35 +1,11 @@
 import React from 'react'
-import { motion } from "framer-motion";
 
 type Props = {}
 
-export default function About({}: Props) {
+function About({}: Props) {
   return (
-    <div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
-          About
-        </h3>
-        
-        <motion.img
-          initial={{
-            x: -200,
-            opacity: 0,
-          }} 
-          transition={{
-            duration: 1.2,
-          }}
-          whileInView={{
-            opacity: 1,
-            x: 0,  
-          }}
-          viewport={{
-            once: true,
-          }}
-          src="https://media-exp1.licdn.com/dms/image/C4D03AQG3ezW14WyKhQ/profile-displayphoto-shrink_200_200/0/1662836126839?e=1669852800&v=beta&t=UOmovv1TESmdfv8VigmHIiC4tduLsl4JhCF-eSIyF5Q" 
-    
-          className="-mb-20 md:mb-0 flex-shrink-0 rounded-full w-56 h-56 object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
-        />
-    </div>
-  );
+    <div>About</div>
+  )
 }
 
+export default About
