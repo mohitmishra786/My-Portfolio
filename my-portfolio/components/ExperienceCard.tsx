3b0351290230feff7d3fdf3e5326d8eec2dde6e4
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 14:07:54 +0530

    Update

--- my-portfolio/components/ExperienceCard.tsx
+++ my-portfolio/components/ExperienceCard.tsx
@@ -6,7 +6,21 @@ type Props = {}
 function ExperienceCard({}: Props) {
   return (
     <article>
-        <motion.img className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center ' src="https://omdena.com/wp-content/uploads/2021/10/logo.png" alt="" />
+        <motion.img 
+        initial={{
+            y: -100,
+            opacity: 0,
+        }}
+        whileInView={{
+            y: 0,
+            opacity: 1,
+        }}
+        transition={{
+            duration: 1.2,
+        }}
+        className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center ' 
+        src="https://omdena.com/wp-content/uploads/2021/10/logo.png" 
+        alt="" />
     </article>
   )
 }
