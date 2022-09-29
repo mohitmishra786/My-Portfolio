f311e22b63035ccd2e2ca8002f1f2455066931ac
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 20:03:02 +0530

    Update

--- my-portfolio/my-portfolio/schemas/schema.js
+++ my-portfolio/my-portfolio/schemas/schema.js
@@ -1,6 +1,6 @@
 import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
-import pageInfo from './pageInfo'
+
 
 import experience from './experience';
 import pageInfo from './pageInfo';
