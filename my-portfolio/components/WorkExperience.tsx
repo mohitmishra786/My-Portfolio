5c7fcb29d88b3620142422ef8a5fd6fa57012518
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 13:53:57 +0530

    Update

--- my-portfolio/components/WorkExperience.tsx
+++ my-portfolio/components/WorkExperience.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 function WorkExperience({}: Props) {
   return (
-    <div className='h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+    <div className='h-screen flex relative'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
             Experience
         </h3>
