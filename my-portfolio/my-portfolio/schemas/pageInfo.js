f474596691ae45a822022b4eda9fbc14067fbba9
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 19:45:19 +0530

    backend

---
+++ my-portfolio/my-portfolio/schemas/pageInfo.js
@@ -0,0 +1,49 @@
+export default {
+  name: 'pageInfo',
+  title: 'pageInfo',
+  type: 'document',
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "heroImage",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      }
+    },
+    {
+      name: "profilePic",
+      title: "ProfilePic",
+      type: "image",
+      options: {
+        hotspot: true,
+      }
+    },
+    {
+      name: "phoneNumber",
+      title: "PhoneNumber",
+      type: "string",
+    },
+    {
+      name: "email",
+      title: "Email",
+      type: "string",
+    },
+    {
+      name: "address",
+      title: "Address",
+      type: "string",
+    },
+    {
+      name: "socials",
+      title: "Socials",
+      type: "array",
+      of: [{type: "reference", to:{type: "social"}}],
+    }
+  ],
+}
