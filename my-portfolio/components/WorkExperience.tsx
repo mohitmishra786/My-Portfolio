4ee30f2f7de8acb7a124a768fb9f765600d180d4
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 13:48:18 +0530

    Update

---
+++ my-portfolio/components/WorkExperience.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function WorkExperience({}: Props) {
+  return (
+    <div>WorkExperience</div>
+  )
+}
+
+export default WorkExperience
--- my-portfolio/pages/index.tsx
+++ my-portfolio/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import About from '../components/About'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import WorkExperience from '../components/WorkExperience'
 
 
 const Home: NextPage = () => {
@@ -26,7 +27,9 @@ const Home: NextPage = () => {
       </section>
 
       {/* Experience */}
-
+      <section id='experience'>
+        <WorkExperience />
+      </section>
 
       {/* Project */}
 
