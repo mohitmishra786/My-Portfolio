d01381ab4df0aed0c19e181ce9802f891dec1926
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 15:16:20 +0530

    Update

--- my-portfolio/components/Projects.tsx
+++ my-portfolio/components/Projects.tsx
@@ -8,10 +8,10 @@ function Projects({}: Props) {
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
             Projects
         </h3>
-
-        <div className='w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12'>
+        <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
 
         </div>
+        <div className='w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12  ' />
     </div>
   );
 }
