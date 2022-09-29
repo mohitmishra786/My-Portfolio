2359f3989e2e63e39103c2dd76ff1d92b9475363
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 15:00:19 +0530

    Update

--- my-portfolio/pages/index.tsx
+++ my-portfolio/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import About from '../components/About'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import Projects from '../components/Projects'
 import Skills from '../components/Skills'
 import WorkExperience from '../components/WorkExperience'
 
