c07a81165abca1fd259905e69e113cd1cb489c55
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-30 03:24:12 +0530

    Final Touch

--- my-portfolio/components/Projects.tsx
+++ my-portfolio/components/Projects.tsx
@@ -28,6 +28,7 @@ function Projects({projects}: Props) {
         <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]'>
             {projects?.map((project , i) => (
                 <div 
+                    key={project._id}
                     className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
                     <motion.img
                     initial={{
--- my-portfolio/pages/index.tsx
+++ my-portfolio/pages/index.tsx
@@ -27,7 +27,7 @@ const Home = ({pageInfo, experiences, projects, skills, socials} : Props) => {
   return (
     <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
       <Head>
-        <title>Mohit's Portfolio</title>
+        <title>{pageInfo?.name} - Portfolio</title>
       </Head>
 
       {/* Header */}
