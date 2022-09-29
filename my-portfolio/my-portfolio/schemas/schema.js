8a228588745f0ebf4b200cb788ce0d514d7e232b
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 19:59:07 +0530

    update

--- my-portfolio/my-portfolio/schemas/schema.js
+++ my-portfolio/my-portfolio/schemas/schema.js
@@ -2,10 +2,17 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 import pageInfo from './pageInfo'
 
+import experience from './experience';
+import pageInfo from './pageInfo';
+import skill from './skill';
+import social from './social';
+import project from './project';
+
+
 
 export default createSchema({
   name: 'default',
   types: schemaTypes.concat([
     pageInfo,
   ]),
-})
+});
---
+++ my-portfolio/my-portfolio/schemas/social.js
@@ -0,0 +1,18 @@
+export default {
+  name: 'social',
+  title: 'Social',
+  type: 'document',
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      description: "Platform of the Social Media",
+      type: "string",
+    },
+    {
+      name: "url",
+      title: "Url",
+      type: "url",
+    },
+  ],
+};
