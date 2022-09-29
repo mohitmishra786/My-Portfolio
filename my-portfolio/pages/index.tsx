874c01fb649cce66c9b5976cc0d5446fab525a54
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 14:35:50 +0530

    Update

--- my-portfolio/pages/index.tsx
+++ my-portfolio/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import About from '../components/About'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import Skills from '../components/Skills'
 import WorkExperience from '../components/WorkExperience'
 
 
@@ -32,7 +33,7 @@ const Home: NextPage = () => {
       </section>
 
       {/* Skills */}
-      <section id='skills' className='snap-center' >
+      <section id='skills' className='snap-start' >
         <Skills />
       </section>
 
