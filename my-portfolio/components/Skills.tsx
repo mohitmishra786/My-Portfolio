36f2ee3aaa94f5c1d54111c0b9827b6ac4538b0e
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 14:45:00 +0530

    Update

--- my-portfolio/components/Skills.tsx
+++ my-portfolio/components/Skills.tsx
@@ -7,7 +7,15 @@ type Props = {}
 function Skills({}: Props) {
   return (
     <motion.div 
-    
+    initial={{
+        opacity: 0,
+    }}
+    whileInView={{
+        opacity: 1,
+    }}
+    transition={{
+        duration: 1.5,
+    }}
     className='h-screen flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'>
     <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
         Skills
