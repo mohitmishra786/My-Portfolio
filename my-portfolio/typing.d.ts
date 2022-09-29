f444ceb5664d3429d47856c92dc3e0f806a24ffb
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-30 00:44:44 +0530

    update

--- my-portfolio/typing.d.ts
+++ my-portfolio/typing.d.ts
@@ -25,6 +25,41 @@ export interface PageInfo extends SanityBody {
     profilePic: Image;
 }
 
+export interface Technology extends SanityBody {
+    _type: "skill";
+    image: Image;
+    progress: number;
+    title: string;
+}
+
+export interface Skill extends SanityBody {
+    _type: "skill";
+    image: Image;
+    progress: number;
+    title: string;
+}
+
+export interface Experience extends SanityBody {
+    _type: "experience";
+    company: string;
+    companyImage: Image;
+    dateStarted: date;
+    dateEnded: date;
+    isCurrentlyWorkingHere: boolean;
+    jobTitle: string;
+    points: string[];
+    technologies: Technology[];
+}
+
+export interface Project extends SanityBody {
+    title: string;
+    _type: "skill";
+    image: Image;
+    linkToBuild: string;
+    summary: string;
+    technologies: Technology[];
+}
+
 export interface Social extends SanityBody {
     _type: "social",
     title: string;
