424344cd6c7f682f623a55d2bb9b91090a31092c
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 12:52:42 +0530

    Update

--- my-portfolio/components/About.tsx
+++ my-portfolio/components/About.tsx
@@ -5,7 +5,18 @@ type Props = {}
 
 function About({}: Props) {
   return (
-    <div className='h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+    <motion.div 
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1.5,
+      }}
+    
+      className='h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl '>
         About
       </h3>
@@ -30,14 +41,14 @@ function About({}: Props) {
       />
 
       <div className='space-y-10 px-0 md:px-10'>
-        <h4 className='text-4xl font-semibold'>
+        <h4 className='text-3xl font-semibold'>
           Here is a <span className='underline decoration-[#F7AB0A]/50'>Little</span> Background
         </h4>
-        <p>
-          I'm Mohit.❤️ You can also call me as Chessman, here's a little bit about me... I've been coding for over 4 years now.
+        <p className='text-sm xl:text-lg'>
+          I'm Mohit.❤️ You can also call me as Chessman. I'm a Machine learning Developer and a competitive programmer. Most of my time is spent staring at a computer screen. During the day, I am usually programming, working to derive insight from large datasets. My skills include Data Analysis, Data Visualization, Machine learning and Deep Learning. I have developed a strong acumen for problem solving, and I enjoy an occasional challenge. I am able to handle multiple task on daily basis.I always use a creative approach to solve problems.I am always energetic and eager to learn new skills.Even though, i take my work seriously, I do have a good sense of humour. I have done some good projects which uses machine learning, deep learning.  I have completed all my projects in a timely fashion. I have a good hands on experience working with NLP. I believe that words and data are the two most powerful tools to change the world.I love to work with data
         </p>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
