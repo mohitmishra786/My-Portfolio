6809f2455a0a85567ce3b33d996883445d0a8a7a
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 15:01:05 +0530

    Update

--- my-portfolio/components/Projects.tsx
+++ my-portfolio/components/Projects.tsx
@@ -4,7 +4,11 @@ type Props = {}
 
 function Projects({}: Props) {
   return (
-    <div>Projects</div>
+    <div className='h-screen'>
+        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+            Projects
+        </h3>
+    </div>
   )
 }
 
