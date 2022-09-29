03d6a2a20764ee099c76103ec2e682d53dd0f568
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 14:41:35 +0530

    Update

--- my-portfolio/components/Skills.tsx
+++ my-portfolio/components/Skills.tsx
@@ -7,9 +7,12 @@ function Skills({}: Props) {
   return (
     <motion.div 
     
-    className='h-screen'>
+    className='h-screen flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'>
     <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
         Skills
+    </h3>
+    <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>
+       Hover over a skill for current proficiency 
     </h3>
     </motion.div>
   )
