ebb53d05656074311240aeccfee31e9811543a76
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 14:43:23 +0530

    Update

---
+++ my-portfolio/components/Skill.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function Skill({}: Props) {
+  return (
+    <div>Skill</div>
+  )
+}
+
+export default Skill
--- my-portfolio/components/Skills.tsx
+++ my-portfolio/components/Skills.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { motion } from "framer-motion";
+import Skill from './Skill';
 
 type Props = {}
 
@@ -14,6 +15,10 @@ function Skills({}: Props) {
     <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>
        Hover over a skill for current proficiency 
     </h3>
+
+    <div className='grid grid-cols-4 gap-5'>
+        <Skill />
+    </div>
     </motion.div>
   )
 }
