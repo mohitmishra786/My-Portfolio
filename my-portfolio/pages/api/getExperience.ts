f8d3c785a277bf58058e01fee69ef7f9085b8ca8
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-30 00:52:28 +0530

    update

---
+++ my-portfolio/pages/api/getExperience.ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { groq } from "next-sanity";
+import { SanityClient } from "next-sanity";
+import { sanityClient } from "../../sanity";
+import { Experience} from "../../typing";
+
+
+const query = groq `
+    *[_type == "experience"]{
+      ...,
+      technologies[] =>
+    }
+`
+
+type Data = {
+    experiences : Experience[];
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+  ) {
+    const experiences: Experience[] = await sanityClient.fetch(query);
+    
+    res.status(200).json({ experiences })
+  }
+  
---
+++ my-portfolio/pages/api/getProjects.ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { groq } from "next-sanity";
+import { SanityClient } from "next-sanity";
+import { sanityClient } from "../../sanity";
+import { Project} from "../../typing";
+
+
+const query = groq `
+    *[_type == "project"]{
+      ...,
+      technologies[] =>
+    }
+`
+
+type Data = {
+    projects : Project[];
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+  ) {
+    const projects: Project[] = await sanityClient.fetch(query);
+    
+    res.status(200).json({ projects })
+  }
+  
