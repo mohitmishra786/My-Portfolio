95f6e57a03dde9107638d5bc8037990808739d08
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 16:14:53 +0530

    Update

--- my-portfolio/pages/index.tsx
+++ my-portfolio/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import About from '../components/About'
+import ContactMe from '../components/ContactMe'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 import Projects from '../components/Projects'
