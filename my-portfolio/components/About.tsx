5ccfc4abba0e666d5713d98be0b834dc6a6087df
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 12:15:07 +0530

    Update

--- my-portfolio/components/About.tsx
+++ my-portfolio/components/About.tsx
@@ -9,6 +9,24 @@ function About({}: Props) {
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl '>
         About
       </h3>
+      <motion.img 
+      initial={{
+        x: -200,
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+        x: 0,
+      }}
+      transition={{
+        duration: 1.2,
+      }}
+      viewport={{
+        once: true,
+      }}
+      className="img" 
+      src='https://media-exp1.licdn.com/dms/image/C4D03AQG3ezW14WyKhQ/profile-displayphoto-shrink_200_200/0/1662836126839?e=1669852800&v=beta&t=UOmovv1TESmdfv8VigmHIiC4tduLsl4JhCF-eSIyF5Q'
+      />
     </div>
   )
 }
