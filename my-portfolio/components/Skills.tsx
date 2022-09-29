6e6897c5e95abc42b0bfbceae4283348476cb889
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-29 14:38:15 +0530

    Update

--- my-portfolio/components/Skills.tsx
+++ my-portfolio/components/Skills.tsx
@@ -5,9 +5,13 @@ type Props = {}
 
 function Skills({}: Props) {
   return (
-    <div className='h-screen'>
+    <motion.div 
+    
+    className='h-screen'>
+    <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
         Skills
-    </div>
+    </h3>
+    </motion.div>
   )
 }
 
