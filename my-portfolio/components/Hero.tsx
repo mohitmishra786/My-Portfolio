be9c5228b092220b25ea0895db20d88dd48ae488
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2022-09-30 19:12:39 +0530

    cursorColor in hero file

--- my-portfolio/components/Hero.tsx
+++ my-portfolio/components/Hero.tsx
@@ -36,7 +36,7 @@ function Hero({pageInfo}: Props) {
             </h2>
             <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
                 <span className='mr-3'>{text}</span>
-                <Cursor cursorColor="#F7AB0A" />
+                {/* <Cursor cursorColor="#F7AB0A" /> */}
             </h1>
 
             <div className='pt-5'>
